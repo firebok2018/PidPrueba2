26f11a8c4e1acd84e637eeeb72d9daefc081ef4d
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-09-29 20:34:09 -0500

    se cambio x1

--- src/demo/Ejecucion.java
+++ src/demo/Ejecucion.java
@@ -3,13 +3,13 @@ package demo;
 public class Ejecucion {
 
 	public static void main(String[] args) {
-		int x = 1;
+		int x1 = 1;
 		int y = 2;
 		int z = 3;
 		int m = 5050;
 		int q = 2020;
 		
-		System.out.println(x);
+		System.out.println(x1);
 		System.out.println(y);
 		System.out.println(z);
 		System.out.println(m);
