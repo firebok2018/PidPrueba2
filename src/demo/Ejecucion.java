a712e4744a930cbb7d71bbb8962819a10199f7c4
Samuel2v <rvsamuel273@gmail.com>
2020-09-29 20:36:29 -0500

    se agrego la variable "S"

--- src/demo/Ejecucion.java
+++ src/demo/Ejecucion.java
@@ -8,12 +8,14 @@ public class Ejecucion {
 		int z = 3;
 		int m = 5050;
 		int q = 2020;
+		int s = 9920;
 		
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
 		System.out.println(m);
 		System.out.println(q);
+		System.out.println(s);
 			
 	}
 
