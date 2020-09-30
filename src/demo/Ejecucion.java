cb08ce61106f727d09a01cd559b941ded23f96c5
Firebok Playr <39416877+firebok2018@users.noreply.github.com>
2020-09-29 20:48:29 -0500

    Ejecucion.java
    Add

--- src/demo/Ejecucion.java
+++ src/demo/Ejecucion.java
@@ -1,6 +1,77 @@
+<<<<<<< HEAD
 
 package demo;
 
+public class Ejecucion {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		int m = 4;
+		int q = 2020;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(m);
+		System.out.println(q);
+			
+	}
+
+}
+=======
+package demo;
+
+public class Ejecucion {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		int m = 5050;
+		int q = 2020;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(m);
+		System.out.println(q);
+			
+	}
+
+}
+>>>>>>> branch 'master' of https://github.com/jorgejacinto9701/PidPrueba2.git
+=======
+package demo;
+
+<<<<<<< HEAD
+		int x1 = 1;
+		int y = 2;
+		int z = 3;
+
+		int m = 4;
+		int q = 5;
+		int z = 2020;
+
+		int m = 5050;
+		int q = 2020;
+
+		
+		System.out.println(x1);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(m);
+		System.out.println(q);
+
+		System.out.println(z);
+	    
+		System.out.println("Jose Luis Carrillo Tello");
+
+	}
+
+}
+=======
 public class Ejecucion {
 
 	public static void main(String[] args) {
@@ -21,13 +92,12 @@ public class Ejecucion {
 			
 		System.out.println("Rony Lima Loza");
 		System.out.println("Suyin Silva Contreras");
-
+<<<<<<< HEAD
 		System.out.println("Molina Quiroz Franco");
-
+=======
 		System.out.println("Jose Luis Carrillo Tello" + "luchjto");
-		System.out.println("Samuel Rv");
-
+>>>>>>> branch 'master' of https://github.com/jorgejacinto9701/PidPrueba2.git
 	}
 
 }
-
+>>>>>>> branch 'master' of https://github.com/jorgejacinto9701/PidPrueba2.git
