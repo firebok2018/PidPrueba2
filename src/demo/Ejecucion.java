bc2d7150e52508685dcf591e3b0f955692fed9a3
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-09-29 19:35:35 -0500

    Se agrega la variable q

--- src/demo/Ejecucion.java
+++ src/demo/Ejecucion.java
@@ -7,11 +7,13 @@ public class Ejecucion {
 		int y = 2;
 		int z = 3;
 		int m = 4;
+		int q = 5;
 		
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
 		System.out.println(m);
+		System.out.println(q);
 			
 	}
 
